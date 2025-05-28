3360a401f5d97dcfceddafacc4698d43b62999a4
Yizie <giselerodrigues20566@gmail.com>
2025-05-28 17:34:34 -0300

    Corrige host e porta para Render

--- app.py
+++ app.py
@@ -11,4 +11,8 @@ def surpresa():
     return render_template('surpresa.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
